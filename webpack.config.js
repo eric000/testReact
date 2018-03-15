7dadb20bf52c69f07f1ab210cd986ca962b47b0b
eric000 <894639091@qq.com>
2018-03-15 22:04:20 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,34 @@
 const path = require('path')
+const webpack =require('webpack')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 module.exports = {
     entry: './src/app.jsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'js/bundle.js',
+        publicPath: '/dist/'
     },
     plugins: [
+        //html处理
         new htmlWebpackPlugin({
             template: './src/index.html'
         }),
-        new ExtractTextPlugin("styles.css")
+        //抽离css样式（打包在js时，加载时空白长）
+        new ExtractTextPlugin("css/[name].css"),
+        //抽取公共
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
+        })
     ],
+    devServer: {
+        contentBase: './dist',
+        port: 8086
+    },
     module: {
         rules :[
+            //jsx处理
             {
                 test: /\.jsx$/,
                 exclude: /(node_modules)/,
@@ -25,13 +39,49 @@ module.exports = {
                     }
                 }
             },
+            //css处理
             {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
                     fallback: "style-loader",
                     use: "css-loader"
                 })
-           }
+            },
+            //styl处理
+            {
+                test: /\.styl$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    //use: "css-loader!stylus-loader"
+                    use: ['css-loader', 'stylus-loader']
+                })
+            },
+            //图片处理
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            //字体图标处理
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            }
         ]
     }
 }
